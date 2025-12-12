9f7bc608bbf6bc27d3ddbaed44809921483da265
MacBook <macbook@MacBook-Pro-MacBook.local>
2025-12-12 09:31:36 +0500

    style: update calorie scanner intro text

--- bot/calorie_scanner.py
+++ bot/calorie_scanner.py
@@ -20,7 +20,7 @@ def show_calorie_menu(message, bot):
         bot.send_photo(
             message.chat.id,
             photo,
-            caption="🍽 **Kaloriya Skaneri**\n\nKaloriyani qanday aniqlamoqchisiz?",
+            caption="🍽 **Kaloriya Skaneri**\n\nKaloriyani qanday hisoblamoqchisiz?\nRasm yuborasizmi yoki mahsulot nomini yozasizmi?👇🏻",
             reply_markup=markup,
             parse_mode="Markdown"
         )
